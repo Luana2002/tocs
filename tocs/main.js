e11808ae113c1ebe5ee8aee42dcd059ebac1a663
luanaPaulaEdgeUfal <luana.paula@edge.ufal.br>
2022-09-26 12:04:53 -0300

    subir projeto musical completo

---
+++ tocs/main.js
@@ -0,0 +1,33 @@
+function SomToca(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado");
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+listaDeTeclas[0].onclick = SomToca;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    SomToca(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
